15cf9b7c1c2513903664f1ffd3e89691fa2b4d8f
WeiboPan <1525695648@qq.com>
2024-01-20 20:57:01 +0800

    补充

--- SpringMVC/springMVC-demo01/src/main/java/com/atguigu/mvc/controller/HelloController.java
+++ SpringMVC/springMVC-demo01/src/main/java/com/atguigu/mvc/controller/HelloController.java
@@ -16,4 +16,9 @@ public class HelloController {
         //返回视图名称
         return "index";
     }
+
+    @RequestMapping("/target")
+    public String toTarget(){
+        return "target";
+    }
 }
