88816b4e409fb1a05c102bb9058a3d5b2335760d
WeiboPan <1525695648@qq.com>
2024-01-24 22:23:33 +0800

    day05

---
+++ SpringMVC/springMVC-demo3/src/main/java/com/atguigu/mvc/controller/ScopeController.java
@@ -0,0 +1,50 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class ScopeController {
+    @RequestMapping("/testRequestServletAPI")
+    public String  testRequestServletAPI(HttpServletRequest request){
+        request.setAttribute("testRequestScope","hello,servletAPI");
+        return "success";
+    }
+
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView(){
+        ModelAndView modelAndView = new ModelAndView();
+        // 处理模型数据，即向请求域request共享数据
+        modelAndView.addObject("testRequestScope","hello,ModelAndView");
+        // 设置视图名称
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    @RequestMapping("/testModel")
+    public String testModel(Model model){
+        model.addAttribute("testRequestScope","hello,Model");
+        System.out.println(model.getClass().getName());
+        return "success";
+    }
+
+    @RequestMapping("/testMap")
+    public String testMap(Map<String,Object> map){
+       map.put("testRequestScope","hello,Map");
+        System.out.println(map.getClass().getName());
+       return "success";
+    }
+
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap modelMap){
+        modelMap.addAttribute("testRequestScope","hello,ModelMap");
+        System.out.println(modelMap.getClass().getName());
+        return "success";
+    }
+}
