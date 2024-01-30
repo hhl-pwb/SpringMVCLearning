ff7e3745bcf8d29c3c7d121614729b0b7701da01
WeiboPan <1525695648@qq.com>
2024-01-30 22:28:04 +0800

    1

--- SpringMVC/springMVC-demo3/src/main/java/com/atguigu/mvc/controller/ScopeController.java
+++ SpringMVC/springMVC-demo3/src/main/java/com/atguigu/mvc/controller/ScopeController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -45,6 +47,19 @@ public class ScopeController {
     public String testModelMap(ModelMap modelMap){
         modelMap.addAttribute("testRequestScope","hello,ModelMap");
         System.out.println(modelMap.getClass().getName());
+        return "success";
+    }
+
+    @RequestMapping("/testSession")
+    public String testSession(HttpSession session){
+        session.setAttribute("testSessionScope","hello, session");
+        return "success";
+    }
+
+    @RequestMapping("/testApplication")
+    public String testApplication(HttpSession session){
+        ServletContext servletContext = session.getServletContext();
+        servletContext.setAttribute("testApplicationScope","hell,application");
         return "success";
     }
 }
