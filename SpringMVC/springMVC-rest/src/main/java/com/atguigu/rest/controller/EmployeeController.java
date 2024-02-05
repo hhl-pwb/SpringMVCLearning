bcee870562347d11690783954c32d424d7872d4b
WeiboPan <1525695648@qq.com>
2024-02-05 23:13:38 +0800

    1

--- SpringMVC/springMVC-rest/src/main/java/com/atguigu/rest/controller/EmployeeController.java
+++ SpringMVC/springMVC-rest/src/main/java/com/atguigu/rest/controller/EmployeeController.java
@@ -4,9 +4,8 @@ import com.atguigu.rest.bean.Employee;
 import com.atguigu.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,5 +23,28 @@ public class EmployeeController {
         model.addAttribute("employeeList",employeeList);
         return "employee_list";
     }
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        // 删除成功后，和当前的请求已经没有关系了，应该使用重定向的方式跳转页面
+        return "redirect:/employee";
+    }
+    @RequestMapping(value = "/employee",method = RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
 
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee",employee);
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee",method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
 }
