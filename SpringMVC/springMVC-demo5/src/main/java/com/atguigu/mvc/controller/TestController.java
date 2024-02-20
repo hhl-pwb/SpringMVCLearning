6cf39bb59a069c39d607d2d9d2ed5c5867141077
WeiboPan <1525695648@qq.com>
2024-02-20 22:42:28 +0800

    interceptor

---
+++ SpringMVC/springMVC-demo5/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    /**
+     * /** 表示多层路径
+     * @return
+     */
+    @RequestMapping("/**/testInterceptor")
+    public String testInterceptor(){
+        return "success";
+    }
+}
---
+++ SpringMVC/springMVC-demo5/src/main/java/com/atguigu/mvc/interceptors/SecondInterceptor.java
@@ -0,0 +1,40 @@
+package com.atguigu.mvc.interceptors;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 在springMVC.xml中使用<ref bean></ref>的方式引入拦截器的时候，序言将拦截器设置为IOC容器的普通组件
+ * 使用@Component标注,
+ * 使用@Component标注后的组件名默认为类名首字母小写，即secondInterceptor
+ */
+@Component
+public class SecondInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println("SecondInterceptor-->preHandle");
+//        return HandlerInterceptor.super.preHandle(request, response, handler);
+        // 返回false表示拦截，返回true表示放行
+//        return false;
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler
+            , ModelAndView modelAndView) throws Exception {
+        System.out.println("SecondInterceptor-->postHandle");
+//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler
+            , Exception ex) throws Exception {
+        System.out.println("SecondInterceptor-->afterCompletion");
+//        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
